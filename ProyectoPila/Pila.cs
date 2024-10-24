b2202de27f56e384867ffae7cb30fb154cd3e795
beto200519 <norbertocarrillo2005@gmail.com>
2024-10-24 11:43:42 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoPila/Pila.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPila
+{
+    internal class Pila
+    {
+        private int MAX;
+        private int tope = 0;
+        private NODO inicio;
+        Stack<int> a = new Stack<int>();
+        public Pila(int MAX)
+        {
+            this.MAX = MAX;
+            inicio = null;
+        }
+        public bool Empty()
+        {
+            if(inicio == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Full()
+        {
+            if(tope == MAX)
+            {
+                return true;
+            } else
+            { 
+                return false; 
+            }
+        }
+        public bool Push(int num)
+        {
+            if ( !Full() )
+            {
+                a.Push(num);
+                return true; //La pila tiene espacio
+            }
+            else
+            {
+                return false; //La pila está llena
+            }
+        }
+        public int Pop()
+        {
+            if ( !Empty() )
+            {
+                return tope;
+            }
+            return tope;
+            //Regresa el numero sacado de la pila y si está vacía regresa -1
+        }
+        public void PrintStack()
+        {
+            //Imprime la lista
+        }
+    }
+}
---
+++ ProyectoPila/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ProyectoPila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            
+            while (a != false)
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Clear();
+                    Console.WindowHeight = 17;
+                    Console.WindowWidth = 30;
+                    Console.WriteLine("\tProyecto Pilas");
+                    Console.WriteLine("------------------------------");
+                    Console.WriteLine("");
+                    Console.WriteLine("1.-Establecer Tamaño");
+                    Console.WriteLine("");
+                    Console.WriteLine("2.-Push");
+                    Console.WriteLine("");
+                    Console.WriteLine("3.-Pop");
+                    Console.WriteLine("");
+                    Console.WriteLine("4.-Imprimir");
+                    Console.WriteLine("");
+                    Console.WriteLine("5.-Salir");
+                    Console.WriteLine("");
+                    Console.WriteLine("------------------------------");
+                    Console.Write("Seleccionar Opción => ");
+                    int Opción = int.Parse(Console.ReadLine());
+                    switch (Opción)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            Console.WindowHeight = 10;
+                            Console.WindowWidth = 60;
+                            Console.Clear();
+                            a = false;
+                            break;
+                    }
+                }catch(Exception e)
+                {
+                    Console.WindowHeight = 10;
+                    Console.WindowWidth = 60;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Clear();
+                    Console.WriteLine("\n\n\n\n\tCaracter no valido <ENTER PARA CONTINUAR>");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
